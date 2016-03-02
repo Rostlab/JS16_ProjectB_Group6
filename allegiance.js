a76cff804404511365ece769929fdb0269d3c5a5
Thuy Tran <thuy.tran@tum.de>
2016-03-02 17:57:34 +0100

    Fixing allegiances

--- allegiance.js
+++ allegiance.js
@@ -1,8 +1,11 @@
+module.exports ={
 function getAllegiance(date){
-		if(data.search(/^"House/) < 0){
+		if(data.search(/^House/) < 0){
 			return "?";
 		}
-		var ally = date.search(/"House ([A_Z][a-z]+)/)
+		var ally = date.match(/House ([A-Z][a-z]+)/);
+			return ally;
 		
+	}
 }
 
