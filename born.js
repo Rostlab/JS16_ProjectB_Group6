3f6180290534ff789b8255ef75b1ebf93e5e0be1
asesselmann <anna.sesselmann@tum.de>
2016-03-02 17:51:33 +0100

    Correction of attribute born (in)

--- born.js
+++ born.js
@@ -3,7 +3,7 @@ module.exports ={
 		if (date.search(/\d/) < 0){
 			return "?";
 		}
-		int end = (date.search(/AC/)<date.search(/BC/))?(date.search(/AC/)):(date.search(/BC/);
+		var end = (date.search(/AC/)<date.search(/BC/))?(date.search(/AC/)):(date.search(/BC/);
 		date = date.slice(date.search(/\d/),end+2);
 		var born = (date.search(/AC/)>=0)?(500+parseInt(date)):(500-parseInt(date));
 		return born;
