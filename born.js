c337eeda5180abd5a42b3e6e536198490a07fb54
Hack3l <Hack3l@users.noreply.github.com>
2016-03-03 14:15:51 +0100

    Fixxed attribute born

--- born.js
+++ born.js
@@ -1,11 +1,20 @@
 module.exports ={
 	convert_born: function(date){
-		if (date.search(/\d/) < 0){
+		var ac = date.search(/AC/);
+		var bc = date.search(/BC/);
+		if (date.search(/\d/) < 0 | (ac<0 && bc<0)){
 			return "?";
 		}
-		var end = (date.search(/AC/)<date.search(/BC/))?(date.search(/AC/)):(date.search(/BC/));
+		var end;
+		if (ac>=0 && bc>=0){
+			end = (ac<bc)?(ac):(bc);
+		} else if (ac>=0){
+			end = ac;
+		} else {
+			end = bc;
+		}
 		date = date.slice(date.search(/\d/),end+2);
-		var born = (date.search(/AC/)>=0)?(500+parseInt(date)):(500-parseInt(date));
+		var born = (end==ac)?(500+parseInt(date)):(500-parseInt(date));
 		return born;
 	}
 }
