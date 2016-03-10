28fbb1df8a65eda0f92da8f524d7a2f06f738b86
asesselmann <anna.sesselmann@tum.de>
2016-03-10 23:37:23 +0100

    Get data from DB of Project A

---
+++ api-handler/dataAccess.js
@@ -0,0 +1,64 @@
+const https = require("https");
+const all = require("./getAllAllegiances");
+const cha = require("./getAllCharacters");
+const cul = require("./getAllCultures");
+const tit = require("./getAllTitles");
+
+module.exports = {
+	allegiances: function(){
+		https.get('https://got-api.bruck.me/api/houses/', function(res) {
+			console.log(`Got response: ${res.statusCode}`);
+			res.on('data', function(d){
+				var allegiances = all.getAllAllegiances(res);
+				console.log(allegiances);
+			});
+  			// consume response body
+  			res.resume();
+		}).on('error', function(e) {
+  			console.log(`Got error: ${e.message}`);
+		});
+	},
+
+	characters: function(){
+		https.get('https://got-api.bruck.me/api/characters/', function(res) {
+			console.log(`Got response: ${res.statusCode}`);
+			res.on('data', function(d){
+				var characters = cha.getAllCharacters(d);
+				//console.log(characters);
+			});
+			
+  			// consume response body
+  			res.resume();
+		}).on('error', function(e) {
+  			console.log(`Got error: ${e.message}`);
+		});
+	},
+
+	cultures: function() {
+		https.get('https://got-api.bruck.me/api/cultures/', function(res) {
+			console.log(`Got response: ${res.statusCode}`);
+			res.on('data', function(d){
+				var cultures = cul.getAllCultures(d);
+				//console.log(cultures);
+			});
+  			// consume response body
+  			res.resume();
+		}).on('error', function(e) {
+  			console.log(`Got error: ${e.message}`);
+		});
+	},
+
+	titles: function() {
+		https.get('https://got-api.bruck.me/api/characters/', function(res) {
+			console.log(`Got response: ${res.statusCode}`);
+			res.on('data', function(d){
+				var titles = tit.getAllTitles(res);
+				console.log(titles);
+			});
+  			// consume response body
+  			res.resume();
+		}).on('error', function(e) {
+  			console.log(`Got error: ${e.message}`);
+		});
+	}
+}
