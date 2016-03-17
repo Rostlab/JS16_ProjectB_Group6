9784daa042b0d4202863fc80fe52b3aaa1831d29
asesselmann <anna.sesselmann@tum.de>
2016-03-17 22:33:38 +0100

    minor fixes for weka

--- api-handler/getAllCultures.js
+++ api-handler/getAllCultures.js
@@ -3,8 +3,8 @@ module.exports={
 		json = JSON.parse(json);
 		var cultures = [];
 		json.forEach(function(element,index){
-			if(element["name"] != undefined && cultures.indexOf(element["name"]) == -1){
-				cultures.push(element["name"]);
+			if(element["name"] != undefined && cultures.indexOf('"'+element["name"]+'"') == -1){
+				cultures.push('"'+element["name"]+'"');
 			}
 		});
 		return cultures.sort();
