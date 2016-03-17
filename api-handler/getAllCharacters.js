ad55b515e67fb31ba5dd0cabec0a8e2b258e07ba
asesselmann <anna.sesselmann@tum.de>
2016-03-17 17:38:54 +0100

    adding more features, introducing promises

--- api-handler/getAllCharacters.js
+++ api-handler/getAllCharacters.js
@@ -1,7 +1,18 @@
 module.exports={
 	getAllCharacters: function(json){
-	var characters = [];
-	characters = JSON.parse(json);
-			return characters;
+		var characters = [];
+		characters = JSON.parse(json);
+		return characters;
+	},
+
+	getAllNames: function(json){
+		json = JSON.parse(json);
+		var names = [];
+		json.forEach(function(element,index){
+			if(element["name"] != undefined && names.indexOf(element["name"]) == -1){
+				names.push(element["name"]);
+			}
+		});
+		return names.sort();
 	}
 }
